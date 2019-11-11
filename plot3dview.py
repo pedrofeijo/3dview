352a4643af9e9366e2ccf17189c1135a02bbe63c
Pedro Feijó <44382126+pedrofeijo@users.noreply.github.com>
2019-11-11 11:14:49 -0300

    Update plot3dview.py

--- plot3dview.py
+++ plot3dview.py
@@ -7,6 +7,3 @@ v = pptk.viewer(dados) # visualizar em tons de cinza
 v = pptk.viewer(dados, dados[:, 2]) # visualização colorida referencia em Z
 v.set(point_size=0.005)
 
-dados_z= dados[:,2]
-aux= np.mean(dados_z)
-v = pptk.viewer(dados, aux[:,2]) # visualização colorida referencia em Z
