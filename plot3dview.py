90510033abe2d17ccf126a339a861516da7f0088
feijo <pedrofeijo>
2020-02-13 09:48:04 -0300

    novocomite

--- plot3dview.py
+++ plot3dview.py
@@ -1,8 +1,8 @@
 import pptk
 import numpy as np
-arquivo= "/home/feijo/Documents/carvao_ufc/pontos_ida.txt" 
+arquivo= "/home/feijo/Documentos/feijo/carvao_ufc/pontos_1574700886/pontos_1574700886.txt" 
 dados= np.loadtxt(arquivo, delimiter= ' ') #padronizar o formato txt com ou sem virgula dkdkke
-
+dados= dados[:,:3] # ler até a 3 coluna
 v = pptk.viewer(dados) # visualizar em tons de cinza
 v = pptk.viewer(dados, dados[:, 2]) # visualização colorida referencia em Z
 v.set(point_size=0.005)
